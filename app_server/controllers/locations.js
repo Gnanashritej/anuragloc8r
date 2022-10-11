aafcd106b15ced02f0bd572aff7daa7d0908b0d8
gnanashritej <20eg107125@anurag.edu.in>
2022-10-11 19:38:21 +0530

    changes in view templates

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -56,7 +56,7 @@ const locationInfo = (req, res) => {
         title: 'Anurag University',
       },
       sidebar: {
-        context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+        context: 'is a place of Anurag University which has top Courses to learn and fulfill all of your dreams.',
         callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
       },
       location: {
